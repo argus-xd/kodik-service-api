998501ca95eba8b3ecd421b99d4c7cbff7423be7
Argus_xD <xxxARGUSxxx58@gmail.com>
2022-07-08 14:33:25 +0500

    jest token

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import config from '../src/config';
 
-describe('AppController (e2e)', () => {
+describe('Kodik (e2e)', () => {
 	let app: INestApplication;
 
 	beforeEach(async () => {
@@ -15,22 +16,34 @@ describe('AppController (e2e)', () => {
 		await app.init();
 	});
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer())
-			.get('/')
-			.expect(200)
-			.expect('Hello World!');
+
+	describe('token', () => {
+		it('check', async () => {
+			expect(config.clients.kodik.authToken).not.toHaveLength(0);
+		});
 	});
+	describe('search', () => {
+		it('/kodik/search empty title (GET)', async () => {
+			const data = await request(app.getHttpServer())
+				.get('/kodik/search')
+				.expect(200)
+				.then(response => {
+					return JSON.parse(response.text);
+				});
 
-	it('/kodik/search (GET)', async () => {
-		const data = await request(app.getHttpServer())
-			.get('/kodik/search?title=naruto')
-			.expect(200)
-			.then(response => {
-				return JSON.parse(response.text);
-			});
+			expect(data).toHaveLength(0);
+		});
 
-		expect(data).not.toHaveLength(0);
-		expect(data[0]).toHaveProperty('title');
+		it('/kodik/search (GET)', async () => {
+			const data = await request(app.getHttpServer())
+				.get('/kodik/search?title=naruto')
+				.expect(200)
+				.then(response => {
+					return JSON.parse(response.text);
+				});
+
+			expect(data).not.toHaveLength(0);
+			expect(data[0]).toHaveProperty('title');
+		});
 	});
 });
